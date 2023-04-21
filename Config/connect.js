00d0a0bb64dcf4db4e19da5da4202a85e9bddf1d
Suraj <surajbloodstone@gmail.com>
2023-04-21 21:47:27 +0530

    added backend

--- Config/connect.js
+++ Config/connect.js
@@ -4,7 +4,7 @@ async function connect() {
   try {
     mongoose
       .connect(
-        "mongodb+srv://eeeerr00:U9JLBwwQ8dC1zPkx@cluster0.jgnxi3c.mongodb.net/spotify"
+        `mongodb+srv://eeeerr00:${process.env.MONGO_PASSWORD}@cluster0.jgnxi3c.mongodb.net/spotify`
       )
       .then(console.log("connected  to the databasee"));
   } catch (err) {
